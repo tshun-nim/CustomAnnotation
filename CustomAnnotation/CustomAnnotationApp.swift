37f5717a0647a026e19722101403be85f8991d64
tshun <s.t0811@icloud.com>
2024-10-02 21:25:14 +0900

    Initial Commit

---
+++ CustomAnnotation/CustomAnnotationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomAnnotationApp.swift
+//  CustomAnnotation
+//
+//  Created by 田島隼也 on 2024/10/02.
+//
+
+import SwiftUI
+
+@main
+struct CustomAnnotationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
